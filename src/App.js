94b81c171b56be386946043d778429760199e61e
Omkar Arora <aroraomkar@gmail.com>
2021-04-04 18:39:23 +0530

    chore: added dependencies

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
 import "./App.css";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="App">
+      <div className="container-header">
+        asass
+      </div>
+      <main className="main">
+        ayaya
+      </main>
+      <div className="container-footer">
+        kslasmal
+      </div>
+    </div>
+  )
 };
 export default App;
