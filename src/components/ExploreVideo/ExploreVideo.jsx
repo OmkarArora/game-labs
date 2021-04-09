4a49f75a305c6ec335c5f68507b5a9456ec9d029
Omkar Arora <aroraomkar@gmail.com>
2021-04-09 21:14:40 +0530

    feat: added embed video iframe

--- src/components/ExploreVideo/ExploreVideo.jsx
+++ src/components/ExploreVideo/ExploreVideo.jsx
@@ -1,3 +1,25 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { useAllVideos } from "../../contexts/AllVideos/allVideosContext";
+
 export const ExploreVideo = () => {
-	return <div>Video playback here</div>
-}
+  const { videoId } = useParams();
+  const [video, setVideo] = useState({});
+  const { allVideos } = useAllVideos();
+  useEffect(() => {
+    const video = allVideos.find((item) => item.id === videoId);
+    setVideo(video);
+  }, [allVideos, videoId]);
+  return (
+    <div>
+      <iframe
+        style={{ width: "80%", height: "200px" }}
+        src={video.video}
+        title="YouTube video player"
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+      ></iframe>
+    </div>
+  );
+};
