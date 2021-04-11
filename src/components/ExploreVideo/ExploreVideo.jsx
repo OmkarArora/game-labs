bd2376e1bf993710b9b84c2a62a15e3b8ae9cbf3
Omkar Arora <aroraomkar@gmail.com>
2021-04-11 17:58:07 +0530

    style: video player scaling

--- src/components/ExploreVideo/ExploreVideo.jsx
+++ src/components/ExploreVideo/ExploreVideo.jsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { useAllVideos } from "../../contexts/AllVideos/allVideosContext";
 
+import "./exploreVideo.css";
+
 export const ExploreVideo = () => {
   const { videoId } = useParams();
   const [video, setVideo] = useState({});
@@ -11,15 +13,17 @@ export const ExploreVideo = () => {
     setVideo(video);
   }, [allVideos, videoId]);
   return (
-    <div>
+    <div className="explore-video">
       <iframe
-        style={{ width: "80%", height: "200px" }}
         src={video.video}
         title="YouTube video player"
         frameBorder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
         allowFullScreen
       ></iframe>
+      <div className="video-info">
+        {video.description}
+      </div>
     </div>
   );
 };
--- src/contexts/AllVideos/mockData.js
+++ src/contexts/AllVideos/mockData.js
@@ -6,7 +6,7 @@ export const data = [
 		subcategory: {type: "Agent", name: "Viper"},
 		thumbnail: "https://i.ytimg.com/vi/6JGEmOoR_x4/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBy0Pg7kFLMAg9-cghsGIb5GChEHg",
 		video: "https://www.youtube.com/embed/6JGEmOoR_x4",
-		descripton: "New viper tricks on patch 2.06",
+		description: "New viper tricks on patch 2.06",
 		runtime: {minutes: 25, seconds: 18}
 	},
 	{
@@ -16,7 +16,7 @@ export const data = [
 		subcategory: {type: "Agent", name: "Yoru"},
 		thumbnail: "https://i.ytimg.com/vi/P8eq61v7WKU/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLACc5iVfTYUZp2C5wSg_-S0i5tRjg",
 		video: "https://www.youtube.com/embed/P8eq61v7WKU",
-		descripton: "New viper tricks on patch 2.06",
+		description: "New viper tricks on patch 2.06",
 		runtime: {minutes: 9, seconds: 45}
 	},
 	{
@@ -26,7 +26,7 @@ export const data = [
 		// subcategory: {type: "Agent", name: "Yoru"},
 		thumbnail: "https://i.ytimg.com/vi/6MerElOo2eA/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAdwJ5-kjfLguOBGveM1uykHHt8rQ",
 		video: "https://www.youtube.com/embed/6MerElOo2eA",
-		descripton: "In this video, I showcase 11 advanced techniques that always work in Fortnite Battle Royale.",
+		description: "In this video, I showcase 11 advanced techniques that always work in Fortnite Battle Royale.",
 		runtime: {minutes: 13, seconds: 26}
 	},
 ]
