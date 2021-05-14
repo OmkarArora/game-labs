8f38759045efdb4ae95f6ea034946b856689e790
Omkar Arora <aroraomkar@gmail.com>
2021-05-14 15:51:08 +0530

    fix: removed pre filled login details

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -7,8 +7,8 @@ import "./auth.css";
 import { fetchPlaylists } from "../../api/playlists.api";
 
 export const Login = () => {
-  const [email, setEmail] = useState("user@gmail.com");
-  const [password, setPassword] = useState("abcd@1234");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const [passwordVisible, setPasswordVisibility] = useState(false);
 
   const { isUserLoggedIn, loginUserWithCredentials, appState } = useAuth();
