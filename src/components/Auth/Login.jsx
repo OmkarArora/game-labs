517cbe4949cd77f0de490868d33d0c30fdf02910
Omkar Arora <aroraomkar@gmail.com>
2021-05-17 19:24:56 +0530

    refactor: removed prefilled login details

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -7,8 +7,8 @@ import { fetchPlaylists, fetchUserSubscriptions } from "../../api";
 import "./auth.css";
 
 export const Login = () => {
-  const [email, setEmail] = useState("user@gmail.com");
-  const [password, setPassword] = useState("abcd@1234");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const [passwordVisible, setPasswordVisibility] = useState(false);
 
   const { isUserLoggedIn, loginUserWithCredentials, appState } = useAuth();
